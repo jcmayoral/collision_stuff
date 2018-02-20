0fe7c14fc5bb6628048550cdebcb4c487d193986
jcmayoral <jocamaba1989@gmail.com>
2018-02-20 21:58:45 +0100

    compiling version

--- collision_detection_nav/collision_detector_diagnoser/ros/include/collision_detector_diagnoser/custom_filter.hpp
+++ collision_detection_nav/collision_detector_diagnoser/ros/include/collision_detector_diagnoser/custom_filter.hpp
@@ -4,6 +4,7 @@
 class CustomMessageFilter{
   CustomMessageFilter();
   ~CustomMessageFilter();
+public:
 
   void stop(){
     delete [] collision_flags_;
@@ -25,16 +26,16 @@ class CustomMessageFilter{
   void registerCallback(int input_number){
 
     for (int i=0; i != input_number; ++i){
-      ros::Subscriber tmp_subscribe;  //blabla
-      ros::Subscriber subLeft = nh.subscribe<sensor_msgs::Image> ("/collisions" + str(i), 10,boost::bind(&subscribe, _1, i));
+      ros::Subscriber tmp_subscribe = nh_.subscribe<fusion_msgs::sensorFusionMsg> ("/collisions" +std::to_string(i), 10,boost::bind(&CustomMessageFilter::subscribeCB,this, _1, i));
     }
 
   }
 
-  void subscribe(const fusion_msgs::sensorFusionMsgConstPtr& detectors, int index){
+  void subscribeCB(const fusion_msgs::sensorFusionMsgConstPtr& detector, int index){
   }
 
 private:
   double timeout_;
+  ros::NodeHandle nh_;
   bool *collision_flags_;
 };
